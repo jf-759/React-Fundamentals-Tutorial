ab7d2d530e9da7c7aa5e08bb1755d07f1201bc86
jf-759 <jane.fernandez@students.dominican.edu>
2025-02-23 22:30:45 -0800

    completed part 2 of PO2

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 
 function POPOSSpace() {
     return (
         <div>
-            <img src={logo} width="300" height="300" alt="Hello" />
+            <img src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`} 
+            width="300" 
+            height="300" 
+            alt="50 California St." 
+            />
             <h1>Name...</h1>
             <div>Address...</div>
         </div>
