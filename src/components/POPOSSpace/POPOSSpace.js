84bde68586d49cfd151110e60dbda2b05aaf5f24
jf-759 <jane.fernandez@students.dominican.edu>
2025-02-25 14:18:10 -0800

    completed styling spaces

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -12,16 +12,15 @@ function POPOSSpace(props) {
             alt="Hello" 
             />
             <h1>
-                <Link to={`/details/${id}`}>
+                <Link 
+                    className="POPOSSpace-title" 
+                    to={`/details/${id}`}>
                     {name}
                 </Link>
             </h1>
-            <div>{address}</div>
-            <div>{hours}</div>
-            <div>
-                <Link to={`/details/${id}`}>
-                    <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-                </Link>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div>{hours}</div>
             </div>
         </div>
     )
